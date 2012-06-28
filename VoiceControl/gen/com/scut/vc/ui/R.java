8154eb52f5f8ff72a74632429e0d1967daa00000
QiuJingLiang <532277343@qq.com>
2012-06-28 15:39:27 +0800

    Signed-off-by: QiuJingLiang <532277343@qq.com>

--- VoiceControl/gen/com/scut/vc/ui/R.java
+++ VoiceControl/gen/com/scut/vc/ui/R.java
@@ -9,24 +9,22 @@ package com.scut.vc.ui;
 
 public final class R {
     public static final class array {
-        public static final int preference_entries_iat_engine=0x7f070008;
-        public static final int preference_entries_iat_rate=0x7f07000c;
-        public static final int preference_entries_isr_engine=0x7f07000e;
-        public static final int preference_entries_isr_rate=0x7f070010;
-        public static final int preference_entries_tts_music=0x7f070006;
-        public static final int preference_entries_tts_role=0x7f070004;
-        public static final int preference_values_iat_engine=0x7f070009;
-        public static final int preference_values_iat_rate=0x7f07000d;
-        public static final int preference_values_isr_engine=0x7f07000f;
-        public static final int preference_values_isr_rate=0x7f070011;
-        public static final int preference_values_poi_city=0x7f07000b;
-        public static final int preference_values_poi_province=0x7f07000a;
-        public static final int preference_values_tts_music=0x7f070007;
-        public static final int preference_values_tts_role=0x7f070005;
-        public static final int searchEngineValue_list_preference=0x7f070003;
-        public static final int searchEngine_list_preference=0x7f070002;
-        public static final int voiceEngineValue_list_preference=0x7f070001;
-        public static final int voiceEngine_list_preference=0x7f070000;
+        public static final int entries_list_preference=0x7f070000;
+        public static final int entriesvalue_list_preference=0x7f070001;
+        public static final int preference_entries_iat_engine=0x7f070006;
+        public static final int preference_entries_iat_rate=0x7f07000a;
+        public static final int preference_entries_isr_engine=0x7f07000c;
+        public static final int preference_entries_isr_rate=0x7f07000e;
+        public static final int preference_entries_tts_music=0x7f070004;
+        public static final int preference_entries_tts_role=0x7f070002;
+        public static final int preference_values_iat_engine=0x7f070007;
+        public static final int preference_values_iat_rate=0x7f07000b;
+        public static final int preference_values_isr_engine=0x7f07000d;
+        public static final int preference_values_isr_rate=0x7f07000f;
+        public static final int preference_values_poi_city=0x7f070009;
+        public static final int preference_values_poi_province=0x7f070008;
+        public static final int preference_values_tts_music=0x7f070005;
+        public static final int preference_values_tts_role=0x7f070003;
     }
     public static final class attr {
     }
@@ -40,25 +38,31 @@ public final class R {
         public static final int text_introduction_size=0x7f050003;
     }
     public static final class drawable {
-        public static final int btn_bg=0x7f020000;
-        public static final int btn_bg_focused=0x7f020001;
-        public static final int btn_bg_normal=0x7f020002;
-        public static final int btn_bg_pressed=0x7f020003;
-        public static final int chatbg=0x7f020004;
-        public static final int chatbgb=0x7f020005;
-        public static final int clock=0x7f020006;
-        public static final int delete=0x7f020007;
-        public static final int helper_voice=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int icon=0x7f02000a;
-        public static final int icon_iat_demo=0x7f02000b;
-        public static final int icon_isr_demo=0x7f02000c;
-        public static final int icon_tts_demo=0x7f02000d;
-        public static final int logo=0x7f02000e;
-        public static final int mywb=0x7f02000f;
-        public static final int titlebar_bg=0x7f020010;
-        public static final int wave=0x7f020011;
-        public static final int window_bg=0x7f020012;
+        public static final int assistant=0x7f020000;
+        public static final int assistant_play_normal=0x7f020001;
+        public static final int assistant_play_pressed=0x7f020002;
+        public static final int btn_bg=0x7f020003;
+        public static final int btn_bg_focused=0x7f020004;
+        public static final int btn_bg_normal=0x7f020005;
+        public static final int btn_bg_pressed=0x7f020006;
+        public static final int chatbg=0x7f020007;
+        public static final int chatbgb=0x7f020008;
+        public static final int chatfrom_bg_normal=0x7f020009;
+        public static final int chatto_bg_voice_normal=0x7f02000a;
+        public static final int clock=0x7f02000b;
+        public static final int delete=0x7f02000c;
+        public static final int helper_voice=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int icon=0x7f02000f;
+        public static final int icon_iat_demo=0x7f020010;
+        public static final int icon_isr_demo=0x7f020011;
+        public static final int icon_tts_demo=0x7f020012;
+        public static final int logo=0x7f020013;
+        public static final int mywb=0x7f020014;
+        public static final int titlebar_bg=0x7f020015;
+        public static final int user=0x7f020016;
+        public static final int wave=0x7f020017;
+        public static final int window_bg=0x7f020018;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f0a0003;
@@ -71,6 +75,7 @@ public final class R {
         public static final int clockTime=0x7f0a0009;
         public static final int deleteTime=0x7f0a000a;
         public static final int helper_voice=0x7f0a0010;
+        public static final int imageView1=0x7f0a0000;
         public static final int linearLayout2=0x7f0a0006;
         public static final int seekbar=0x7f0a0012;
         public static final int seekbar_container=0x7f0a0011;
@@ -79,7 +84,6 @@ public final class R {
         public static final int textGroup=0x7f0a000c;
         public static final int title=0x7f0a000e;
         public static final int txvInfo=0x7f0a0001;
-        public static final int txvName=0x7f0a0000;
     }
     public static final class layout {
         public static final int chat_helper=0x7f030000;
