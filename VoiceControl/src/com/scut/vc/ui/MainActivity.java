a7c5525f98473731466065ca516ba5f45a44c864
jackzpj <jackzpj@gmail.com>
2012-07-04 16:45:00 +0800

    Signed-off-by: jackzpj <jackzpj@gmail.com>

--- VoiceControl/src/com/scut/vc/ui/MainActivity.java
+++ VoiceControl/src/com/scut/vc/ui/MainActivity.java
@@ -57,7 +57,7 @@ import com.scut.vc.xflib.ChatAdapter;
 import com.scut.vc.xflib.ChatEng;
 
 public class MainActivity extends Activity implements RecognizerDialogListener,
-OnClickListener {
+		OnClickListener {
 	/** Called when the activity is first created. */
 	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 
@@ -80,9 +80,9 @@ OnClickListener {
 
 	public ProgressBar pd;// 识别中进度条
 
-	public TextView tv; //识别中的文字说明
-	public ImageView iv; //识别中的背景
-	private ImageButton ib; //识别按钮
+	public TextView tv; // 识别中的文字说明
+	public ImageView iv; // 识别中的背景
+	private ImageButton ib; // 识别按钮
 
 	private boolean showProgressDiaglog = false;
 	public static boolean EnableGoogleVoice = false;// 使用google API
@@ -103,16 +103,16 @@ OnClickListener {
 		 * 测度代码;
 		 */
 		updateListView(R.layout.chat_helper, "有什么可以帮到您？");
-		
-		//自启动识别
-		SharedPreferences sharedata_start = getSharedPreferences(
-				"startTurn", MODE_WORLD_READABLE);
+
+		// 自启动识别
+		SharedPreferences sharedata_start = getSharedPreferences("startTurn",
+				MODE_WORLD_READABLE);
 		boolean startTurn = sharedata_start.getBoolean("startTurn", false);// 如果不能正确获取自启动识别的选项，则以“0”为默认值，表示不自启动识别
 		System.out.println("startTurn = " + startTurn);
 
-		if(startTurn){
-			SharedPreferences sharedata1 = getSharedPreferences(
-					"voiceEngine", MODE_WORLD_READABLE);
+		if (startTurn) {
+			SharedPreferences sharedata1 = getSharedPreferences("voiceEngine",
+					MODE_WORLD_READABLE);
 			String voiceEngine = sharedata1.getString("voiceEngine", "1");// 如果不能正确获取语义引擎选项的数据，则以第一项为值
 			System.out.println("voiceEngine = " + voiceEngine);
 
@@ -127,7 +127,6 @@ OnClickListener {
 				// updateListView(R.layout.chat_user, voiceString);
 			}
 		}
-		
 
 		ArrayList<Contact.ContactPerson> callTarget = new ArrayList<Contact.ContactPerson>();// 打电话列表
 		Contact.ContactPerson contactPerson1 = mContact.new ContactPerson(
@@ -142,12 +141,11 @@ OnClickListener {
 		// Task task = new Task(Task.OpenApp, "com.ihandysoft.alarmclock");
 		// Task task = new Task(Task.Search, "com.android.soundrecorder");
 
-
-		// DeviceControl.Device device = mDevCon.new Device("flash", false);
-		// Task task = new Task(Task.SwitchOnDevice, device);
+		 DeviceControl.Device device = mDevCon.new Device("flash", true);
+		 Task task = new Task(Task.SwitchOnDevice, device);
 
 		// Task task = new Task(Task.SetAlarm, "大闹天宫闹钟");
-		// Test(task);
+		 Test(task);
 		// mDevCon.Release();
 		ArrayList<AppsManager.Package_Info> appList = new ArrayList<AppsManager.Package_Info>();
 		AppsManager.Package_Info info1 = mAppManager.new Package_Info("相机",
@@ -157,10 +155,9 @@ OnClickListener {
 		// voiceString = "打开相机";
 		appList.add(info1);
 		appList.add(info2);
-		Task task = new Task(Task.OpenApp, appList);
-		//Test(task);
-		//voiceString = "今天下午五点的闹钟";
-
+		//Task task = new Task(Task.OpenApp, appList);
+		// Test(task);
+		// voiceString = "今天下午五点的闹钟";
 
 	}
 
@@ -192,30 +189,30 @@ OnClickListener {
 		switch (item.getItemId()) {
 		case Menu.FIRST + 1:
 			Toast.makeText(this, "打开设置界面", Toast.LENGTH_SHORT).show();
-		Intent intent2 = new Intent();
-		intent2.setClass(this, SettingActivity.class);
-		startActivity(intent2);
-		break;
+			Intent intent2 = new Intent();
+			intent2.setClass(this, SettingActivity.class);
+			startActivity(intent2);
+			break;
 		case Menu.FIRST + 2:
 			Toast.makeText(this, "打开帮助界面", Toast.LENGTH_SHORT).show();
-		Intent intent1 = new Intent();
-		intent1.setClass(this, HelpActivity.class);
-		startActivity(intent1);
-		break;
+			Intent intent1 = new Intent();
+			intent1.setClass(this, HelpActivity.class);
+			startActivity(intent1);
+			break;
 
 		case Menu.FIRST + 3:
 			Toast.makeText(this, "打开闹钟列表", Toast.LENGTH_SHORT).show();
-		Intent intent3 = new Intent();
-		intent3.setClass(this, AlarmActivity.class);
-		startActivity(intent3);
-		break;
+			Intent intent3 = new Intent();
+			intent3.setClass(this, AlarmActivity.class);
+			startActivity(intent3);
+			break;
 		case Menu.FIRST + 4:
 			Toast.makeText(this, "退出应用程序", Toast.LENGTH_SHORT).show();
-		Intent i = new Intent(MainActivity.this,AlarmService.class);
-		stopService(i);
-		android.os.Process.killProcess(android.os.Process.myPid());
+			Intent i = new Intent(MainActivity.this, AlarmService.class);
+			stopService(i);
+			android.os.Process.killProcess(android.os.Process.myPid());
 
-		break;
+			break;
 		}
 		return false;
 	}
@@ -242,9 +239,9 @@ OnClickListener {
 		 * 语义解析时的progressBar显示和文字说明
 		 */
 
-		pd = (ProgressBar)findViewById(R.id.progressBar1);
-		tv = (TextView)findViewById(R.id.textView1);
-		iv = (ImageView)findViewById(R.id.imageView1);
+		pd = (ProgressBar) findViewById(R.id.progressBar1);
+		tv = (TextView) findViewById(R.id.textView1);
+		iv = (ImageView) findViewById(R.id.imageView1);
 		pd.setVisibility(View.INVISIBLE);
 		tv.setVisibility(View.INVISIBLE);
 		iv.setVisibility(View.INVISIBLE);
@@ -312,7 +309,7 @@ OnClickListener {
 			case Task.CALL: {
 				@SuppressWarnings("unchecked")
 				ArrayList<Contact.ContactPerson> callList = (ArrayList<Contact.ContactPerson>) task
-				.getTaskParam();
+						.getTaskParam();
 				if (0 == callList.size()) {
 					mAppManager.Execute("com.android.contacts");
 				} else if (1 == callList.size()) {
@@ -322,12 +319,12 @@ OnClickListener {
 					ShowContactSelectDialog(callList, task);
 				}
 			}
-			break;
+				break;
 			case Task.SendMessage: {
 
 				@SuppressWarnings("unchecked")
 				ArrayList<Contact.ContactPerson> msgList = (ArrayList<Contact.ContactPerson>) task
-				.getTaskParam();
+						.getTaskParam();
 				if (0 == msgList.size()) {
 					// mAppManager.Execute("com.android.contacts");
 				} else if (1 == msgList.size()) {
@@ -337,7 +334,7 @@ OnClickListener {
 					ShowContactSelectDialog(msgList, task);
 				}
 			}
-			break;
+				break;
 			case Task.OpenApp: {
 
 				ArrayList<AppsManager.Package_Info> appList = (ArrayList<AppsManager.Package_Info>) task
@@ -359,24 +356,24 @@ OnClickListener {
 				}
 
 			}
-			break;
+				break;
 			case Task.Search: {
 				String search = (String) task.getTaskParam();
 				mWebSearch.Execute(search);
 			}
-			break;
+				break;
 			case Task.SwitchOnDevice: {
 				DeviceControl.Device device = (DeviceControl.Device) task
 						.getTaskParam();
 				mDevCon.Execute(device);
 			}
-			break;
+				break;
 			case Task.SetAlarm: {
 				String strvoice = (String) task.getTaskParam();
 				Alarm alarm = new Alarm(MainActivity.this, strvoice);
 				alarm.Execute();
 			}
-			break;
+				break;
 			case Task.Weather: {
 				HashMap weatherInfos = (HashMap) task.getTaskParam();
 				String city = (String) weatherInfos.get("city");
@@ -398,7 +395,7 @@ OnClickListener {
 					e.printStackTrace();
 				}
 			}
-			break;
+				break;
 			case Task.ShowProcess: {
 				if (!showProgressDiaglog) {
 					pd.setVisibility(View.VISIBLE);
@@ -419,7 +416,7 @@ OnClickListener {
 					showProgressDiaglog = false;
 				}
 			}
-			break;
+				break;
 			case Task.IdentifyError: {
 
 				// speakString("对不起哦，找不到你的命令");
@@ -577,16 +574,16 @@ OnClickListener {
 		builder.setTitle("请选择").setItems(items,
 				new android.content.DialogInterface.OnClickListener() {
 
-			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-				ArrayList<Contact.ContactPerson> _list = new ArrayList<Contact.ContactPerson>();
-				_list.add(list.get(which));
-				Task _task = new Task(task.getTaskID(), _list);
-				Message msg = new Message();
-				msg.obj = _task;
-				mhandler.sendMessage(msg);
-			}
-		});
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						ArrayList<Contact.ContactPerson> _list = new ArrayList<Contact.ContactPerson>();
+						_list.add(list.get(which));
+						Task _task = new Task(task.getTaskID(), _list);
+						Message msg = new Message();
+						msg.obj = _task;
+						mhandler.sendMessage(msg);
+					}
+				});
 
 		AlertDialog dialog = builder.create();
 		dialog.show();
@@ -609,16 +606,16 @@ OnClickListener {
 		builder.setTitle("请选择").setItems(items,
 				new android.content.DialogInterface.OnClickListener() {
 
-			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-				ArrayList<AppsManager.Package_Info> _list = new ArrayList<AppsManager.Package_Info>();
-				_list.add(list.get(which));
-				Task _task = new Task(task.getTaskID(), _list);
-				Message msg = new Message();
-				msg.obj = _task;
-				mhandler.sendMessage(msg);
-			}
-		});
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						ArrayList<AppsManager.Package_Info> _list = new ArrayList<AppsManager.Package_Info>();
+						_list.add(list.get(which));
+						Task _task = new Task(task.getTaskID(), _list);
+						Message msg = new Message();
+						msg.obj = _task;
+						mhandler.sendMessage(msg);
+					}
+				});
 
 		AlertDialog dialog = builder.create();
 		dialog.show();
@@ -628,7 +625,6 @@ OnClickListener {
 	@Override
 	protected void onRestart() {
 		// TODO Auto-generated method stub
-		mDevCon.initialTorch();
 		super.onRestart();
 	}
 
@@ -674,10 +670,10 @@ OnClickListener {
 		return mDevCon;
 	}
 
-	public boolean onKeyDown(int keyCode, KeyEvent event){
-		if(keyCode == KeyEvent.KEYCODE_BACK){
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
 			MainActivity.this.finish();
-			Intent i = new Intent(MainActivity.this,AlarmService.class);
+			Intent i = new Intent(MainActivity.this, AlarmService.class);
 			stopService(i);
 
 			Log.v("Work", "MainActivity End");
